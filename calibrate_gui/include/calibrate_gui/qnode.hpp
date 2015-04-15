b4769e283efebeb7ca62bc2c0636db71fa0782fb
Chris Lewis <drchrislewis@gmail.com>
2015-04-15 15:58:08 -0500

    caputure whats on this machine

---
+++ calibrate_gui/include/calibrate_gui/qnode.hpp
@@ -0,0 +1,71 @@
+/**
+ * @file /include/calibrate_gui/qnode.hpp
+ *
+ * @brief Communications central!
+ *
+ * @date February 2011
+ **/
+/*****************************************************************************
+** Ifdefs
+*****************************************************************************/
+
+#ifndef calibrate_gui_QNODE_HPP_
+#define calibrate_gui_QNODE_HPP_
+
+/*****************************************************************************
+** Includes
+*****************************************************************************/
+
+#include <ros/ros.h>
+#include <string>
+#include <QThread>
+#include <QStringListModel>
+
+
+/*****************************************************************************
+** Namespaces
+*****************************************************************************/
+
+namespace calibrate_gui {
+
+/*****************************************************************************
+** Class
+*****************************************************************************/
+
+class QNode : public QThread {
+    Q_OBJECT
+public:
+	QNode(int argc, char** argv );
+	virtual ~QNode();
+	bool init();
+	bool init(const std::string &master_url, const std::string &host_url);
+	void run();
+
+	/*********************
+	** Logging
+	**********************/
+	enum LogLevel {
+	         Debug,
+	         Info,
+	         Warn,
+	         Error,
+	         Fatal
+	 };
+
+	QStringListModel* loggingModel() { return &logging_model; }
+	void log( const LogLevel &level, const std::string &msg);
+
+Q_SIGNALS:
+	void loggingUpdated();
+    void rosShutdown();
+
+private:
+	int init_argc;
+	char** init_argv;
+	ros::Publisher chatter_publisher;
+    QStringListModel logging_model;
+};
+
+}  // namespace calibrate_gui
+
+#endif /* calibrate_gui_QNODE_HPP_ */
